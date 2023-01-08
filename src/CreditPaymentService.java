a342c200c7d7a664647879eee7cd792c594da625
Koloshko Evgeni <kolohkoe@gmail.com>
2023-01-08 16:20:07 +0700

    The calculator is ready

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,11 +1,10 @@
 public class CreditPaymentService {
 
-public double calculate(double sum,double time,double percent)
-{
-        double rate =percent/100/time;
+    public double calculate(double sum, double time, double percent) {
+        double rate = percent / 100 / time;
 
-        double payment =(rate*(Math.pow((1+rate), time)/(Math.pow((1+rate), time)-1)))*1_000_000;
+        double payment = (rate * (Math.pow((1 + rate), time) / (Math.pow((1 + rate), time) - 1))) * 1_000_000;
         return payment;
 
-}
+    }
 }
